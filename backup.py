773f8e4c80e4c289fc37426dc0e8b8655c21b949
Harivansh <hello@harivan.sh>
2021-04-22 20:25:41 +0400

    refactor backup script

--- backup.py
+++ backup.py
@@ -1,28 +1,22 @@
-"""
-This script is in charge of backing up the current user's data.
-"""
+#!/usr/bin/env python3
 
-import logging
 import os
 import tarfile
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from collections import namedtuple
-from datetime import datetime
+from datetime import date, datetime
 from pathlib import Path
 from types import SimpleNamespace
+from typing import Tuple
+
+from utilities import cd
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        format='[%(levelname)s] [%(asctime)s]: %(msg)s',
-        level=logging.INFO
-    )
-
-    today = datetime.today().date()
+    today: date = datetime.today().date()
     parser = namedtuple('Parser', ('main', 'subparser', 'subparsers'))(
         main := ArgumentParser(
             prog='backup',
             description='A script to backup important files.',
-            epilog='Goodbye, world!',
             allow_abbrev=False
         ),
         main.add_subparsers(
@@ -38,7 +32,6 @@ if __name__ == '__main__':
     parser.subparsers.all = parser.subparser.add_parser(
         'all',
         description='Backup all the necessary files.',
-        epilog='Backup all!',
         add_help=True,
         allow_abbrev=False
     )
@@ -49,7 +42,7 @@ if __name__ == '__main__':
     )
     parser.subparsers.all.add_argument(
         '--filename',
-        default=f'{today.year}_{today.month:02d}_{today.day:02d}.tar.gz',
+        default=f'backup__{today.year}_{today.month:02d}_{today.day:02d}.tar.gz',
         help='The name of the tar archive.'
     )
 
@@ -57,12 +50,11 @@ if __name__ == '__main__':
     parser.subparsers.list = parser.subparser.add_parser(
         'list',
         description='List all the files to back up.',
-        epilog='List all!',
         add_help=True,
         allow_abbrev=False
     )
 
-    directories = (
+    files: Tuple = (
         Path(f'{Path.home()}/.gnupg'),
         Path(f'{Path.home()}/.password-store'),
         Path(f'{Path.home()}/.ssh'),
@@ -70,14 +62,14 @@ if __name__ == '__main__':
         Path(f'{Path.home()}/.zshenv'),
         Path(f'{Path.home()}/.zshrc'),
         Path(f'{Path.home()}/.backup'),
-        Path(f'{Path.home()}/Storage/Documents')
+        Path(f'{Path.home()}/Storage/Documents'),
     )
 
-    for directory in directories:
-        if not directory.exists():
-            raise RuntimeError(f'The file {directory} does not exist.')
+    for file in files:
+        if not file.exists():
+            raise RuntimeError(f'The file {file} does not exist.')
 
-    arguments = parser.main.parse_args()
+    arguments: Namespace = parser.main.parse_args()
 
     if arguments.action == 'all':
         if not Path(arguments.destination).is_dir():
@@ -86,17 +78,18 @@ if __name__ == '__main__':
         if (filepath := Path(f'{arguments.destination}/{arguments.filename}')).is_file():
             raise RuntimeError(f'The file: {arguments.filename} already exists in {arguments.destination}.')
 
-        logging.info(f'Backing up the data to: {filepath}')
+        print(f'Backing up the data to: {filepath}...')
 
         with tarfile.open(filepath, 'w:gz') as backup_file:
-            for directory in directories:
-                logging.info(f'Backing up {directory}...')
-                backup_file.add(directory)
+            for file in files:
+                with cd(file.parent):
+                    print(f'Backing up {file}...')
+                    backup_file.add(file.name)
 
-        logging.info('Backup complete.')
+        print('Backup complete.')
 
     elif arguments.action == 'list':
         print('The following files/directories will be backed up:')
 
-        for directory in directories:
-            print(f'{directory}')
+        for file in files:
+            print(f'{file}')
