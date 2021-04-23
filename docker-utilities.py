30545533c829fd0ba6dc8384bb8f4c10a1e2da2b
Harivansh <hello@harivan.sh>
2021-04-23 10:12:28 +0400

    general refactoring

--- docker-utilities.py
+++ docker-utilities.py
@@ -1,24 +1,16 @@
-"""
-The purpose of this script is to manage docker resources.
-"""
+#!/usr/bin/env python3
 
-import logging
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from collections import namedtuple
 from subprocess import run, PIPE
 from types import SimpleNamespace
+from typing import Tuple
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        format='[%(levelname)s] [%(asctime)s]: %(msg)s',
-        level=logging.INFO
-    )
-
     parser = namedtuple('Parser', ('main', 'subparser', 'subparsers'))(
         main := ArgumentParser(
             prog='docker-utilities',
             description='A script for high-level docker-management.',
-            epilog='Goodbye, world!',
             allow_abbrev=False
         ),
         main.add_subparsers(
@@ -34,7 +26,6 @@ if __name__ == '__main__':
     parser.subparsers.purge = parser.subparser.add_parser(
         'purge',
         description='Purge all docker images, containers, volumes, and networks.',
-        epilog='Purge all!',
         add_help=True,
         allow_abbrev=False
     )
@@ -43,32 +34,31 @@ if __name__ == '__main__':
     parser.subparsers.status = parser.subparser.add_parser(
         'status',
         description='Get the status of all docker images, containers, volumes, and networks.',
-        epilog='Status!',
         add_help=True,
         allow_abbrev=False
     )
 
-    arguments = parser.main.parse_args()
+    arguments: Namespace = parser.main.parse_args()
 
     if arguments.action == 'status':
-        logging.info('** Containers **')
+        print('** CONTAINERS **')
         run(('docker', 'container', 'ls', '--all'))
         print()
 
-        logging.info('** Volumes **')
+        print('** VOLUMES **')
         run(('docker', 'volume', 'ls'))
         print()
 
-        logging.info('** Images **')
+        print('** IMAGES **')
         run(('docker', 'image', 'ls', '--all'))
         print()
 
-        logging.info('** Networks **')
+        print('** NETWORKS **')
         run(('docker', 'network', 'ls'))
         print()
 
     elif arguments.action == 'purge':
-        containers = tuple(
+        containers: Tuple[str, ...] = tuple(
             container.strip() for container in
             run(
                 ('docker', 'container', 'ls', '--all', '--quiet'),
@@ -76,15 +66,15 @@ if __name__ == '__main__':
             ).stdout.decode('utf-8').split()
         )
 
-        logging.info('Stopping all containers...')
+        print('Stopping all containers...')
         for container in containers:
             run(('docker', 'container', 'stop', container))
 
-        logging.info('Removing all containers...')
+        print('Removing all containers...')
         for container in containers:
             run(('docker', 'container', 'rm', '--force', container))
 
-        volumes = tuple(
+        volumes: Tuple[str, ...] = tuple(
             volume.strip() for volume in
             run(
                 ('docker', 'volume', 'ls', '--quiet'),
@@ -92,11 +82,11 @@ if __name__ == '__main__':
             ).stdout.decode('utf-8').split()
         )
 
-        logging.info('Removing all volumes...')
+        print('Removing all volumes...')
         for volume in volumes:
             run(('docker', 'volume', 'rm', '--force', volume))
 
-        images = tuple(
+        images: Tuple[str, ...] = tuple(
             image.strip() for image in
             run(
                 ('docker', 'image', 'ls', '--all', '--quiet'),
@@ -104,11 +94,11 @@ if __name__ == '__main__':
             ).stdout.decode('utf-8').split()
         )
 
-        logging.info('Removing all images...')
+        print('Removing all images...')
         for image in images:
             run(('docker', 'image', 'rm', '--force', image))
 
-        networks = tuple(
+        networks: Tuple[str, ...] = tuple(
             network.strip() for network in
             run(
                 ('docker', 'network', 'ls', '--quiet'),
@@ -116,6 +106,6 @@ if __name__ == '__main__':
             ).stdout.decode('utf-8').split()
         )
 
-        logging.info('Removing all networks...')
+        print('Removing all networks...')
         for network in networks:
             run(('docker', 'network', 'rm', network))
