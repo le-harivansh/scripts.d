270deab1a03e1ce12d3b5045918b503a4d70254d
Harivansh <hello@harivan.sh>
2022-07-14 13:41:26 +0400

    feat: setup cleaning script

--- clean/script.zsh
+++ clean/script.zsh
@@ -22,5 +22,52 @@ then
   esac
 fi
 
-# cleaning script start...
-echo 'clean'
+
+# Bleachbit
+
+echo ""
+echo "Cleaning the system-space using bleachbit..."
+echo ""
+sudo bleachbit --clean $(bleachbit --list-cleaners | sed '/system\.memory/d' | sed '/system\.free_disk_space/d')
+echo ""
+echo ""
+
+echo ""
+echo "Cleaning the user-space using bleachbit..."
+echo ""
+bleachbit --clean $(bleachbit --list-cleaners | sed '/system\.memory/d' | sed '/system\.free_disk_space/d')
+echo ""
+echo ""
+
+
+# System
+
+echo ""
+echo "Cleaning journalctl..."
+echo ""
+sudo journalctl --vacuum-time=30days --vacuum-size=50M
+echo ""
+echo ""
+
+
+# Pacman
+
+echo ""
+echo "Removing orphan packages..."
+echo ""
+sudo pacman --remove --nosave --recursive $(pacman --query --unrequired --deps --quiet)
+echo ""
+echo ""
+
+echo ""
+echo "Clearing pacman's cache..."
+echo ""
+sudo pacman --sync --clean --clean
+echo ""
+echo ""
+
+
+echo ""
+echo "Cleanup complete."
+echo ""
+echo ""
