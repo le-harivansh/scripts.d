b21184f33215b6aa0b350eaa64c34ffc33404449
Harivansh <hello@harivan.sh>
2022-08-02 11:20:14 +0400

    feat: add docker cleaning command
    Also refactor logging messages

--- clean.zsh
+++ clean.zsh
@@ -1,53 +1,35 @@
 #! /usr/bin/env zsh
 
-# Bleachbit
+set -e
 
-echo ""
+# Bleachbit
 echo "Cleaning the system-space using bleachbit..."
-echo ""
 sudo bleachbit --clean $(bleachbit --list-cleaners | sed --expression '/system\.memory/d' --expression '/system\.free_disk_space/d')
 echo ""
-echo ""
 
-echo ""
 echo "Cleaning the user-space using bleachbit..."
-echo ""
 bleachbit --clean $(bleachbit --list-cleaners | sed --expression '/system\.memory/d' --expression '/system\.free_disk_space/d')
 echo ""
-echo ""
-
 
 # System
-
-echo ""
 echo "Cleaning journalctl..."
-echo ""
 sudo journalctl --vacuum-time=30days --vacuum-size=50M
 echo ""
-echo ""
-
 
 # Pacman
-
-echo ""
 echo "Removing orphan packages..."
-echo ""
 sudo pacman --remove --nosave --recursive $(pacman --query --unrequired --deps --quiet)
 echo ""
-echo ""
 
-echo ""
 echo "Clearing pacman's cache..."
-echo ""
 sudo pacman --sync --clean --clean
 echo ""
+
+local docker_utilities_script="docker.zsh"
+
+# docker
+echo "Cleaning docker..."
+"${0:a:h}/${docker_utilities_script}" purge
 echo ""
 
-
-# clean docker
-
-
-echo ""
 echo "Cleanup complete."
-echo ""
-echo ""
