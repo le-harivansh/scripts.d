ba67b5193616a1adb22b6ab014bae66862402a2e
Harivansh <hello@harivan.sh>
2024-01-05 12:55:49 +0400

    fix!: rewrite for mac OS

--- backup/backup.sh
+++ backup/backup.sh
@@ -9,7 +9,7 @@ set +a
 
 [ -z "${BACKUP_GPG_ENCRYPTION_PASSPHRASE}" ] && echo "The BACKUP_GPG_ENCRYPTION_PASSPHRASE variable should be set." && exit 1
 
-TEMPORARY_WORKING_DIRECTORY="$(mktemp -d)"
+TEMPORARY_WORKING_DIRECTORY="$(mktemp --directory --tmpdir="/tmp")"
 
 echo "Going into ${TEMPORARY_WORKING_DIRECTORY}..."
 pushd "${TEMPORARY_WORKING_DIRECTORY}"
@@ -21,7 +21,7 @@ echo "Decrypting passwords..."
 shopt -s globstar # enable globstar
 for ENCRYPTED_PASSWORD_FILE in "${PASSWORD_STORE_PATH}"/**/*.gpg
 do
-  PASSWORD_PATH="$(echo "${ENCRYPTED_PASSWORD_FILE}" | sed --expression "s#${PASSWORD_STORE_PATH}/##g" --expression 's#\.gpg##g')"
+  PASSWORD_PATH="$(echo "${ENCRYPTED_PASSWORD_FILE}" | sed -e "s#${PASSWORD_STORE_PATH}/##g" -e 's#\.gpg##g')"
 
   echo "${PASSWORD_PATH} $(pass "${PASSWORD_PATH}")" >> ${PASSWORDS_BACKUP_FILE}
 done
@@ -30,7 +30,7 @@ shopt -u globstar # disable globstar
 BACKUP_FILE="$(date '+%Y-%m-%d.%s').tar.bz2"
 
 echo "Backing up files:"
-tar --dereference --create --verbose --bzip2 --exclude '.directory' --file "${BACKUP_FILE}" ${PASSWORDS_BACKUP_FILE} --directory "${HOME}" Documents Pictures
+tar --dereference --create --verbose --bzip2 --file "${BACKUP_FILE}" ${PASSWORDS_BACKUP_FILE} "${HOME}"/Documents "${HOME}"/Pictures
 
 echo "Encrypting backup file..."
 gpg --sign --symmetric --no-symkey-cache --batch --pinentry-mode loopback --passphrase "${BACKUP_GPG_ENCRYPTION_PASSPHRASE}" "${BACKUP_FILE}"
